557de5b90122f802ba37fb580144235af4dec235
rsncyln <ersanceylan35@gmail.com>
2014-07-20 23:07:58 +0300

    port number initialized

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var logfmt = require("logfmt");
+var port = Number(process.env.PORT || 5000);
 var app = express()
   , http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
-server.listen(process.env.PORT);
+server.listen(port);
 
 
 var game = require('./lib/gameManager.js');
